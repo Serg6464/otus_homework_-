c8954b71dc343041f2888b55aa6884164a3fb71d
Serg6464 <115687761+Serg6464@users.noreply.github.com>
2025-01-23 11:22:04 +0400

    7 test (1,2,1)->(1)

--- tests/tests.cc
+++ tests/tests.cc
@@ -31,12 +31,28 @@ TEST(DDD, CheckNoRoots)
   EXPECT_EQ(retVal,etalonVal);  
 }
 
-// 3. тест проверяющий что для уравнения x^2+1=0 два корня
+// 5. тест проверяющий что для уравнения x^2+1=0 два корня 1,-1
 TEST(DDD, Check2Roots)
 {
   //arrange
   double a = 1.0, b = 0.0, c=-1.0;
   std::vector <double> etalonVal = {1.0, -1.0};
+  //act
+  auto retVal = TestClass::solve(a,b,c);
+  //assert
+  EXPECT_EQ( retVal.size(), etalonVal.size() );
+  for(int i=0; i<retVal.size(); i++ )
+  { 
+    EXPECT_DOUBLE_EQ(retVal[i],etalonVal[i]);
+  }  
+}
+
+// 7. тест проверяющий что для уравнения x^2+2x+1=0 один корень -1
+TEST(DDD, Check1Root)
+{
+  //arrange
+  double a = 1.0, b = 2.0, c=1.0;
+  std::vector <double> etalonVal = {-1.0};
   //act
   auto retVal = TestClass::solve(a,b,c);
   //assert
